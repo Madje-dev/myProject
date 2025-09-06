78ddbbea4c799bdb81837d0496a789b7f62e2c3c
Dhurian <dhurian@outlook.com>
2025-09-06 13:00:12 +0200

    created tasks.json file that has linker information

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 // This will be used to draw a windows with openGL
 
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
-int main (){
-    printf("Hello world \n");
+int main ()
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()){
+
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return -1;
+    }
+            /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
+
 
 }
