5f98b28669950d187b7a4af5f952181dbfe6e5f4
madjeist <dhurian@outlook.com>
2025-10-04 16:19:45 +0200

    created Shade files

--- src/include/Shader/shader.h
+++ src/include/Shader/shader.h
@@ -1,3 +1,23 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#include </home/dhurianvitoldas/Programming/myProject/src/include/glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+    public:
+        unsigned int ID;
+        Shader(const char* vertexPath, const char* fragmentPath);
+        void use();
+        void setBool(const std::string &name, bool value) const;
+        void setInt(const std::string &name, bool value) const;
+        void setFloat(const std::string &name, bool value) const;
+    };   
+
+#endif
+
