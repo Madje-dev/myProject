0fae2435ff0037fc8d458f79706df94a9fdbba3e
madjeist <dhurian@outlook.com>
2025-10-04 21:00:40 +0200

    Adjusted the Z axis on the coordinated but nothing changed.

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,10 @@ int main() {
 
 float vertices[] = {
     // positions          // colors           // texture coords
-     0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
+     0.5,  0.5, -1.0,   1.0, 0.0, 0.0,   1.0, 1.0,   // top right
      0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0, 0.0,   // bottom right
     -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0, 0.0,   // bottom left
-    -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top left 
+    -0.5,  0.5, 1.0,   1.0, 1.0, 0.0,   0.0, 1.0    // top left 
 };
 
     unsigned int VBO;
